30d5e5cc7ee7ef874e23f1d874fca4d9fd53f0a4
Joanna Tracy <joanna98146@yahoo.com>
2017-09-14 13:23:20 +0000

    per kens response, dropped db recreated then migrated, now its working

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
- 
-ActiveRecord::Schema.define(version: 20170913161333) do
+ActiveRecord::Schema.define(version: 20170913175505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +31,5 @@ ActiveRecord::Schema.define(version: 20170913161333) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-
 
 end
